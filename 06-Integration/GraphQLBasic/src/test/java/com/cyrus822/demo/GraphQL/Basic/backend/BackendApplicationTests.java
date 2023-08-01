e09ecf7b392e1c6d88420aefc25e8418f9801f5f
Cyrus Cheng <cyrus@cyrus822.com>
2023-08-01 22:29:19 +0800

    add new graphql demo - basic

---
+++ 06-Integration/GraphQLBasic/src/test/java/com/cyrus822/demo/GraphQL/Basic/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyrus822.demo.GraphQL.Basic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
