42ea62b37fcd04930238546a81bee001abb58f57
Cyrus Cheng <cyrus_kc_cheng@manulife.com>
2025-01-14 15:50:12 +0800

    add devcontainer setup + Redis use case

---
+++ 09-Session-Mgt/src/main/java/com/cyrus822/demo/redis/session/SessionApplication.java
@@ -0,0 +1,13 @@
+package com.cyrus822.demo.redis.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionApplication.class, args);
+	}
+
+}
