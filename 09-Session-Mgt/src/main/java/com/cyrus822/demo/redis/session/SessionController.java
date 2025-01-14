c4175f4505185e40dd02ced0cc4c27874683c62e
Cyrus Cheng <cyrus_kc_cheng@manulife.com>
2025-01-14 17:32:56 +0800

    add k8s config

--- 09-Session-Mgt/src/main/java/com/cyrus822/demo/redis/session/SessionController.java
+++ 09-Session-Mgt/src/main/java/com/cyrus822/demo/redis/session/SessionController.java
@@ -4,17 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 @RequestMapping("/session")
 public class SessionController {
 
     private static final String SESSIONKEY = "myKey";
+	
+    @Value("${POD_NAME:unknown}")
+    private String podName;
+
+    @Value("${POD_IP:unknown}")
+    private String podIp;	
 
     @GetMapping("/set/{value}")
     public String setSessionAttribute(HttpSession session, @PathVariable("value") String value, ModelMap map) {
         session.setAttribute(SESSIONKEY, value);
         map.addAttribute("value", value);
+		map.addAttribute("pod", podName + ":" + podIp);
         return "index";
     }
 
@@ -23,6 +31,7 @@ public class SessionController {
         String value = (String) session.getAttribute(SESSIONKEY);
         value = (value == null) ? "No value" : value;
         map.addAttribute("value", value);
+		map.addAttribute("pod", podName + ":" + podIp);
         return "index";
     }
 }
